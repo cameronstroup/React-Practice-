ea5dfaf11e7f435183f01f320d98cc6118043113
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-06-15 11:27:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// ReactDOM.render(
+//   <h1 className="song">Say my name say my name</h1>,
+//   //   <ul>
+//   //     <li>Cameron is cool </li>
+//   //     <li>Thing 2</li>
+//   //   </ul>,
+//   document.getElementById("root")
+// );
+
+// // const h1 = createElement("h1");
+// // h1.textContent = "cameorn should not have eaten that";
+// // h1.className = "song";
+// // document.getElementById("root").append(h1);
+// const lake = (
+//   <div>
+//     <h1>My awesome website in React</h1>
+//     <h3>Reasons I love React</h3>
+//     <ol>
+//       <li>It's composable</li>
+//       <li>It's declarative</li>
+//       <li>It's a hireable skill</li>
+//       <li>It's actively maintained by skilled people</li>
+//     </ol>
+//   </div>
+// );
+// const page = (
+//   <div>
+//     <h1>Lake the good dog</h1>
+//     <p>Lake is the best dog in the world voted by god himself</p>
+//     <h2>Qualites include</h2>
+//     <uL>
+//       <li>Fast</li>
+//       <li>Smart</li>
+
+//       <li>Good Looking</li>
+//     </uL>
+//   </div>
+// );
+
+// // ReactDOM.render(page, document.getElementById("root"));
+
+// const page = (
+//   <div>
+//     <img src="./react-logo.png" width="40px" />
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Was first released in 2013</li>
+//       <li>Was originallly created by Jodran Walker</li>
+//       <li>is maintained by facebook</li>
+//       <li>Power thousands of enterprise apps, including mobile apps</li>
+//     </ul>
+//   </div>
+// );
+
+// ReactDOM.render(page, document.getElementById("root"));
